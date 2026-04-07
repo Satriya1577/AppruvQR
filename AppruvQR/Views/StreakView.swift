22cf772f6a97763a43a0e7e97746cb8a46d2f617
Satriya <sa@gmail.com>
2026-04-07 22:58:33 +0700

    change text

---
+++ AppruvQR/Views/StreakView.swift
@@ -0,0 +1,18 @@
+//
+//  StreakView.swift
+//  AppruvQR
+//
+//  Created by Satriya Handha Wibowo on 07/04/26.
+//
+
+import SwiftUI
+
+struct StreakView: View {
+    var body: some View {
+        Text("Hello, bowoo!")
+    }
+}
+
+#Preview {
+    StreakView()
+}
