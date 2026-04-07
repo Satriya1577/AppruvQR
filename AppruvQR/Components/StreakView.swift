1d477dbac78984be92f2f7669003670762b54a4c
Satriya <sa@gmail.com>
2026-04-07 22:34:06 +0700

    create streakview page file

---
+++ AppruvQR/Components/StreakView.swift
@@ -0,0 +1,18 @@
+//
+//  StreakView.swift
+//  AppruvQR
+//
+//  Created by Satriya Handha Wibowo on 07/04/26.
+//
+
+import SwiftUI
+
+struct StreakView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StreakView()
+}
